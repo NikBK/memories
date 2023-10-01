f4f21e11aafe40de8d13afb4ff407e8febb531b3
NikBK <nikhilbalakrishna5@gmail.com>
2023-10-01 20:46:08 +0530

    auto creation of public folder if it doesn't exist

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import fs from 'fs';
+import path from 'path';
+
 
 import postRoutes from "./routes/posts.js";
 import assetRoutes from "./routes/assets.js";
@@ -18,6 +21,21 @@ app.use(express.static('./public'));
 app.use("/posts", postRoutes);
 app.use("/public", assetRoutes)
 app.get("/", (req, res) => res.send("Welcome to Memories backend server"));
+
+function createDirectories(pathname) {
+    const __dirname = path.resolve();
+    pathname = pathname.replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, ''); // Remove leading directory markers, and remove ending /file-name.extension
+    if (!fs.existsSync(pathname)) {
+        fs.mkdir(path.resolve(__dirname, pathname), { recursive: true }, e => {
+            if (e) {
+                console.error(e);
+            } else {
+                console.log('Successfully created public directory');
+            }
+        });
+    }
+}
+createDirectories("./public/images");
 
 const PORT = process.env.PORT || 5000;
 const MONGOOSE_CONECTION_URL = process.env.MONGOOSE_CONECTION_URL;
